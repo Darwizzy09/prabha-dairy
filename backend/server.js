2afb21bc77e0f227290fccdff1c414ff21e49ed3
Darwizzy09 <rohitliverpool777@gmail.com>
2026-05-21 17:46:18 +0530

    Fixed CORS to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // 👉 CORS MUST BE THE VERY FIRST THING
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://prabha-dairy.vercel.app'], 
+  origin: ['http://localhost:5173', 'https://prabha-dairy-store.vercel.app'], 
   credentials: true
 }));
 
