0c39f02951d40ad42bb127017d34f68e13d07548
Darwizzy09 <rohitliverpool777@gmail.com>
2026-05-21 18:38:51 +0530

    Added isAdmin to Mongoose Schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
   // Add these right below your password field
   resetPasswordToken: String,
   resetPasswordExpire: Date,
